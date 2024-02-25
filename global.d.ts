24db64fae6577c7c3a9b56134892322071b79188
VandeurenGlenn <vandeurenglenn@gmail.com>
2024-02-25 19:37:20 +0100

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,16 +1,25 @@
-import { BigNumber} from '@ethersproject/bignumber';
+import { BigNumber } from '@ethersproject/bignumber';
 
 declare interface msg {
-  sender: address
-  call(contract: address, method: string, parameters: any[]): Promise<any>
-  staticCall(contract: address, method: string, parameters?: any[]): Promise<any>
-  delegate(contract: address, method: string, parameters: any[]): Promise<any>
-  staticDelegate(contract: address, method: string, parameters?: any[]): Promise<any>
+  sender: address;
+  contract: address;
+  call(contract: address, method: string, parameters: any[]): Promise<any>;
+  staticCall(
+    contract: address,
+    method: string,
+    parameters?: any[]
+  ): Promise<any>;
+  delegate(contract: address, method: string, parameters: any[]): Promise<any>;
+  staticDelegate(
+    contract: address,
+    method: string,
+    parameters?: any[]
+  ): Promise<any>;
 }
 
 declare global {
-  const BigNumber: BigNumber
-  type BigNumberish =  string | number |  bigint | BigNumber | ArrayLike<number>
+  const BigNumber: BigNumber;
+  type BigNumberish = string | number | bigint | BigNumber | ArrayLike<number>;
   type address = string;
-  const msg: msg
+  const msg: msg;
 }
